c698f6c3a7ffda4ca61b504592d82c621d5ae4b9
chav1961 <chav1961@mail.ru>
2023-01-26 14:49:34 +0500

    Daily commit

--- src/main/java/chav1961/csce/Application.java
+++ src/main/java/chav1961/csce/Application.java
@@ -1,5 +1,7 @@
 package chav1961.csce;
 
+
+// https://burakkanber.com/blog/machine-learning-full-text-search-in-javascript-relevance-scoring/
 public class Application {
 
 	public static void main(String[] args) {
